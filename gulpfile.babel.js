ee7668cf71190f97255b7e28b735d80a6e5e22f4
Fabio Leonardo Fuentes <fabioleon.fuentes@gmail.com>
2021-02-11 03:51:42 -0500

    Se crean archivos de estilos (para variables, el index y para el RWD) se crean carpetas para assets y js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,15 +9,20 @@ const browserSync = require('browser-sync')
 const server = browserSync.create()
 
 gulp.task('sass', function () {
-  return gulp
-    .src('dev/scss/**/*.scss')
+  return gulp.src('dev/scss/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'  
+    }))
     .pipe(
       sass({
         pretty: true,
       })
     )
-    .pipe(gulp.dest('./public/'));
-});
+
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(server.stream())
+})
+
 gulp.task('pug', () => {
   return gulp.src('./dev/*.pug')
     .pipe(pug({
@@ -30,6 +35,8 @@ gulp.task('default', () => {
   server.init({
     server: './public'
   })
+  
+  gulp.watch('dev/scss/**/*.scss', gulp.series('sass'))
 
   //PUG
   gulp.watch('./dev/*.pug', gulp.series('pug')).on('change', server.reload)
