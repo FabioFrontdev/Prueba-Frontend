75e063c5a56643b6fa0765d2faa2ce469712a22e
Fabio Leonardo Fuentes <fabioleon.fuentes@gmail.com>
2021-02-11 16:00:10 -0500

    Se actualiza configuración de gulpfile.babel.js, tambien del package.json y se crea la carpeta components para un manejo apropiado del código

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,8 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const pug = require('gulp-pug')
+const babel = require("gulp-babel")
+const uglify = require("gulp-uglify")
 const plumber = require('gulp-plumber')
 
 const browserSync = require('browser-sync')
@@ -24,20 +26,35 @@ gulp.task('sass', function () {
 })
 
 gulp.task('pug', () => {
-  return gulp.src('./dev/*.pug')
+  return gulp.src('./dev/components/*.pug')
     .pipe(pug({
       pretty: true
     }))
     .pipe(gulp.dest('./public/'))
 })
 
+gulp.task("babel", () => {
+  return gulp
+    .src("./dev/js/*.js")
+    .pipe(plumber())
+    .pipe(
+      babel({ presets: [ "@babel/preset-env" ] })
+    )
+    .pipe(concat("scripts-min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./public/js/"))
+})
+
 gulp.task('default', () => {
   server.init({
     server: './public'
   })
-  
+  //SCSS
   gulp.watch('dev/scss/**/*.scss', gulp.series('sass'))
 
   //PUG
   gulp.watch('./dev/*.pug', gulp.series('pug')).on('change', server.reload)
+
+  //JS
+  gulp.watch("./dev/js/*.js", gulp.series('babel')).on('change', server.reload)
 })
